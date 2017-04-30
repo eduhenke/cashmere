146666ae433b10a89f086339671eb944cc681a37
Bruno M. Pacheco <mpacheco.bruno@gmail.com>
2017-04-29 21:24:58 -0300

    They see me flowin' they hatin' (#5)
    * int 90h as reset button
    
    Proper reset procedure implemented. init went to main code to enable
    entrance in the main loop after it has been called without generating
    stack overflow
    
    * configInt macro vector, procedure_offset
    
    configInt enables int vector to be handled by the procedure at
    procedure_offset
    
    * configInt 91h & minor fixes
    
    * int 92h to finish and barcode_read++
    
    int 92h prints total line. barcode_read does what it needs to do.
    toString vars are more general.
    
    * Align

--- CashRegister.asm
+++ CashRegister.asm
@@ -8,16 +8,18 @@ data segment
     ; add your data here!
     string db "print test$"
     pkey db "press any key...$"
-    barcode db 2
+    barcode db 0
     item_name dw 0      ; item's name offset
     item_price dw 0
-    item_price_string db "     "
-    item_price_r db "   $"  ; item price in reais
-    item_price_c db "  $"	; itme price in centavos
+    price_string db "     "
+    price_r db "   $"  ; item price in reais
+    price_c db "  $"	; itme price in centavos
 
     total_price dw 0
     test_string db "Teste...$"
     remaining_letters db 0
+    header db " SEJA BEM VINDO $"
+    total db "total$"
     
     ;Estrutura dos dados
     ;IDs           0           1          2
@@ -30,20 +32,35 @@ stack segment
 ends
 
 code segment
-    call init
-
+    call sys_setup
+    
+    init:
+		clear_printer
+		clear_display
+		
+		printerln header, 0
+  	
+  		; config int 90h
+  		push cx
+  		
+  		mov cx, offset reset
+  		configInt 90h, cx
+		mov cx, offset barcode_read
+		configInt 91h, cx
+		mov cx, offset finish
+		configInt 92h, cx
+		
+		pop cx
+		
+		sti		; Since reset doesn't IRET
 	
-	get_product barcode
-	add_total
-	printline
-	display_word total_price
 	
-	mov barcode, 3
-	get_product barcode
-	add_total
-	printline
-	display_word total_price
+	
+	main_loop:
+		mov cx, 2
+		loop main_loop
 
+	;
 
     jmp sys_exit
 ends
